c76fdd4b4b88baa4c10d345ef189a3f1d48e426e
Dongwy <dwwwwy@bupt.edu.cn>
2020-09-01 12:24:59 +0800

    little_change

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
-import header from '../Layout/header'
+import header from './Layout/header'
 // import UserCreate from '../users/UserCreate'
 // import UserLogin from '../users/UserLogin'
 // import HelloWorld from '../components/HelloWorld'
